a8e79a2bc6494c5c6bd1c6430377f6415e65ef17
Nate <etlf74@yandex.ru>
2022-10-16 22:17:48 +0500

    Added M01_FindMin

---
+++ M01_FindMin/Program.cs
@@ -0,0 +1,16 @@
+﻿// Поиск минимума
+
+int FindMin(int[] array)
+
+{
+    int min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min) min = array[i];
+    }
+    return min;
+}
+
+int[] array = { 11, 8, 10, 2, 5, 6, 7, 3, 4, 25 };
+int min = FindMin(array);
+Console.WriteLine($"Минимальное число равно {min}");
