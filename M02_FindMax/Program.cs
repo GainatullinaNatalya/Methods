7e3e0e9b5ef6bbde1c6469f05f300f5f171f2fb2
Nate <etlf74@yandex.ru>
2022-10-16 22:25:16 +0500

    Added M02_FindMax

---
+++ M02_FindMax/Program.cs
@@ -0,0 +1,16 @@
+﻿// Поиск максимума
+
+int FindMax(int[] array)
+
+{
+    int max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+    }
+    return max;
+}
+
+int[] array = { 113, 8, 100, 2, 55, 6, 723, 3, 40, 25 };
+int max = FindMax(array);
+Console.WriteLine($"Максимальное число равно {max}");
