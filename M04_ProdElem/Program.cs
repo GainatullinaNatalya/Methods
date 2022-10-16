ca48e986ea27d7b7a9b7553540e9c0d3da3acde5
Nate <etlf74@yandex.ru>
2022-10-16 23:15:27 +0500

    Added M04_ProdElem

---
+++ M04_ProdElem/Program.cs
@@ -0,0 +1,16 @@
+﻿// Поиск произведения элементов массива
+
+int ProdElem(int[] array)
+
+{
+    int prod = 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        prod = prod * array[i];
+    }
+    return prod;
+}
+
+int[] array = { 5, 2, 6, 1, 3, 4 };
+int prod = ProdElem(array);
+Console.WriteLine($"Произведение элементов массива равно {prod}");
