7c081f880dd41ac2b3af26f0c1a9c732caa2fd8a
Nate <etlf74@yandex.ru>
2022-10-16 23:03:08 +0500

    Added M03_SumElements

---
+++ M03_SumElements/Program.cs
@@ -0,0 +1,16 @@
+﻿// Поиск суммы элементов массива
+
+int SumElements(int[] array)
+
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int[] array = { 1, 8, 2, 5, 6, 7, 3, 4 };
+int sum = SumElements(array);
+Console.WriteLine($"Сумма элементов массива равна {sum}");
