6575bfa4ecafca6a58fe99b7599719f3eb51ad0c
Nate <etlf74@yandex.ru>
2022-10-17 16:04:06 +0500

    Added M201_ExpoNumb

---
+++ M201_ExpoNumb/Program.cs
@@ -0,0 +1,16 @@
+﻿// Функцию, которая вычисляет число a в степени n
+
+
+int ExpoNumb (int a, int n)
+{
+    if(n == 1) return a;
+    else return a * ExpoNumb(a, n - 1);
+}
+
+Console.WriteLine("Введите число: ");
+int numA = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите степень числа: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine(ExpoNumb(numA, n));
