099806aa18c8f24ff8dd41a571ef13e17c3b9bcd
Nate <etlf74@yandex.ru>
2022-10-17 13:32:37 +0500

    Added M08_ArithMean

---
+++ M08_ArithMean/Program.cs
@@ -0,0 +1,16 @@
+﻿// Среднее арифметическое элеметов массива
+
+double ArithMean(int[] array)
+{
+    double sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum = sum + array[i];
+    }
+    double S = sum / array.Length;
+    return S;
+}
+
+int[] array = { 2, 5, 6, 3, 8, 10};
+double medium = ArithMean(array);
+Console.WriteLine($"Среднее арифметическое элеметов массива: {medium}");
