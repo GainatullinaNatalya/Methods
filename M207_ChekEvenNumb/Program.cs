bcd255631fdd75e55f4757f280829e3ac4b242bf
Nate <etlf74@yandex.ru>
2022-10-17 20:57:00 +0500

    Added M207_CheckEvenNumb

---
+++ M207_ChekEvenNumb/Program.cs
@@ -0,0 +1,13 @@
+﻿// Функцию, определяющую является ли число чётным, то есть возвращающую true, если число чётное, иначе - false
+
+bool CheckEvenNumb(int value)
+{
+    bool numb = false;
+    if (value%2 == 0 && value !=0) numb = true;
+    return numb;
+}
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+bool findNumb = CheckEvenNumb(number);
+Console.WriteLine(findNumb);
