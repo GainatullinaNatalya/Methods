eabe94f5cd052376a4b8a2ed3750c0721e087c1f
Nate <etlf74@yandex.ru>
2022-10-17 22:21:50 +0500

    Added changes M04

--- M04_ProdElem/Program.cs
+++ M04_ProdElem/Program.cs
@@ -1,7 +1,6 @@
 ﻿// Поиск произведения элементов массива
 
 int ProdElem(int[] array)
-
 {
     int prod = 1;
     for (int i = 0; i < array.Length; i++)
@@ -11,6 +10,15 @@ int ProdElem(int[] array)
     return prod;
 }
 
-int[] array = { 5, 2, 6, 1, 3, 4 };
+int size = new Random().Next(5, 10);
+int[] array = new int[size];
+
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 10);
+
+for (int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]} ");
+Console.WriteLine();
+
 int prod = ProdElem(array);
 Console.WriteLine($"Произведение элементов массива равно {prod}");
