9ef59580cbc584979371b93e2a1403fddc484703
Nate <etlf74@yandex.ru>
2022-10-17 14:52:14 +0500

    Added M13_CountOddNum

---
+++ M13_CountOddNum/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчёт количества нечётных элементов в массиве
+
+int CountOddNum(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i]%2 == 1) count++;
+    }
+    return count;
+}
+
+int[] array = { 5, 2, 6, 1, 3, 4, 22, 5, 2, 5, 8, 20, 8 };
+int count = CountOddNum(array);
+Console.WriteLine($"Количество нечетных элементов в массиве равно: {count}");
