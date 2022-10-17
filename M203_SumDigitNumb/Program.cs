1e70f7b34efb1534c52796ba7b772e53d916cc63
Nate <etlf74@yandex.ru>
2022-10-17 16:53:56 +0500

    Added M23_ SumDigitNumb

---
+++ M203_SumDigitNumb/Program.cs
@@ -0,0 +1,20 @@
+﻿// Функцию, которая вычисляет сумму цифр произвольного целого числа n
+
+int SumDigitNumb(int n)
+{
+    int sum = 0;
+    while (n > 0)
+    {
+        int digit = n % 10;
+        sum += digit;
+        n = n / 10; ;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int sum = SumDigitNumb(n);
+
+Console.WriteLine($"Сумма цифр = {sum}");
