bcb86890ada41032d284943b44d9b64d12486bc8
Nate <etlf74@yandex.ru>
2022-10-17 11:54:01 +0500

    Added M07_PrintArray

---
+++ M07_PrintArray/Program.cs
@@ -0,0 +1,15 @@
+﻿// Печать массива на экран
+
+void PrintArray(int[] array)
+{
+    for (int i= 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+int size = 10;
+int[]array = new int[size];
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(1, 10);
+
+PrintArray(array);
