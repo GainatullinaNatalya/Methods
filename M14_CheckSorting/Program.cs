1adda2be127f71dbdb14e2c6c8c198b5a26e12d5
Nate <etlf74@yandex.ru>
2022-10-17 15:33:00 +0500

    Added M14_CheckSorting

---
+++ M14_CheckSorting/Program.cs
@@ -0,0 +1,19 @@
+﻿// Проверка является ли массив отсортированным по возрастанию. 
+//Если массив отсортирован, то возвращать true, иначе - false
+
+bool CheckSorting(int[] array)
+{
+    bool checkArray = true;
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        if (array[i] > array[i + 1]) 
+        {
+            checkArray = false;
+        }
+    }
+    return checkArray;
+}
+
+int[] array = { 1, 2, 3, 4, 3, 10, 22, 25 };
+bool sort = CheckSorting(array);
+Console.WriteLine(sort);
