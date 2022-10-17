55843daaacac1db3ff5444400d1813e611c77c55
Nate <etlf74@yandex.ru>
2022-10-17 14:42:26 +0500

    Added M11_CountNeg

---
+++ M11_CountEvenNum/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчёт количества чётных элементов в массиве
+
+int CountEvenNum(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i]%2 == 0) count++;
+    }
+    return count;
+}
+
+int[] array = { 5, 2, 6, 1, 3, 4, 22, 5, 2, 5, 8, 20, 8 };
+int count = CountEvenNum(array);
+Console.WriteLine($"Количество четных элементов в массиве равно: {count}");
