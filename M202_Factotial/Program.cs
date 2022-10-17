faeff2c3e55ca8fdff495fb33e1aa92f7296386b
Nate <etlf74@yandex.ru>
2022-10-17 16:24:15 +0500

    Added M202_Factorial

---
+++ M202_Factotial/Program.cs
@@ -0,0 +1,12 @@
+﻿// Функцию, которая вычисляет факториал числа n
+
+double Factorial (double n)
+{
+    if(n == 1) return 1;
+    else return n * Factorial(n -1);
+}
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine(Factorial(number));
