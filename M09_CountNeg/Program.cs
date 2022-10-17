45dbf31d0c9b4281444d50e9586b9635acaea5f9
Nate <etlf74@yandex.ru>
2022-10-17 22:38:34 +0500

    Added changes M09

--- M09_CountNeg/Program.cs
+++ M09_CountNeg/Program.cs
@@ -3,13 +3,22 @@
 int CountNeg(int[] array)
 {
     int count = 0;
-    for(int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        if(array[i] < 0) count++;
+        if (array[i] < 0) count++;
     }
     return count;
 }
 
-int[] array = { 5, 2, -6, 1, 3, -4, 22, -15, -2, -5, 8, -20 };
+int size = new Random().Next(5, 10);
+int[] array = new int[size];
+
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-10, 10);
+
+for (int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]} ");
+Console.WriteLine();
+
 int count = CountNeg(array);
 Console.WriteLine($"Количество отрицательных элементов в массиве равно: {count}");
