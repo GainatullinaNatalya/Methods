57d48e2dfa6bdb7eb1af83cff187a01d1a6a92ee
Nate <etlf74@yandex.ru>
2022-10-17 11:24:15 +0500

    Added M06_FindElem

---
+++ M06_FindElem/Program.cs
@@ -0,0 +1,14 @@
+﻿// Проверка наличия элемента в массиве. Возвращает true, если элемент в массиве есть, false – нет
+
+bool FindElem(int[] array, int value)
+{
+    bool findElem = false;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == value) findElem = true;
+    }
+    return findElem;
+}
+int[] array = { 5, 2, 6, 1, 3, 4, 22, 15 };
+bool findNumb = FindElem(array, 10);
+Console.WriteLine(findNumb);
