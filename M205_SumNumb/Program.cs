51c03b9d7ec834377116b42b82658575156058d5
Nate <etlf74@yandex.ru>
2022-10-17 20:55:02 +0500

    Added M205_SumNumb

---
+++ M205_SumNumb/Program.cs
@@ -0,0 +1,17 @@
+﻿// Функцию, складывающую два целых числа
+
+int SumNumb(int a, int b)
+{
+    int sum = a + b;
+    return sum;
+}
+
+Console.WriteLine("Введите целое число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите целое число: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+
+int sum = SumNumb(a, b);
+
+Console.WriteLine($"Сумма целых чисел равна {sum}");
