d2f8d6e48a0e2d5a331f076d64937fdd064d7f0a
Nate <etlf74@yandex.ru>
2022-10-18 10:40:35 +0500

    Added M206

---
+++ M206_CheckPrimeNumb/Program.cs
@@ -0,0 +1,19 @@
+﻿// Функцию, определяющую является ли число простым, 
+//то есть возвращающую true, если число простое, иначе - false
+
+bool CheckPrimeNumb(int number)
+{
+    bool find = true;
+    int count = 0;
+    for(int i = 2; i < number / 2; i++)
+    {
+        if(number%i == 0) count ++;
+    }
+    if(count > 0 || number == 1 || number < 0) find = false;
+    return find;
+}
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+bool number = CheckPrimeNumb(n);
+Console.WriteLine(number);
